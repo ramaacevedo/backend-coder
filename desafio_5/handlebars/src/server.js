c3abe29625c552ea668ac33e284342821a12884c
ramaacevedo <ramiroacevedo18@hotmail.com>
2022-10-30 13:12:53 -0300

    desafio_5

---
+++ desafio_5/handlebars/src/server.js
@@ -0,0 +1,24 @@
+const usersRoutes = require('./routes/productos.js');
+const hbs = require('express-handlebars');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', usersRoutes);
+
+app.set('views', './src/views');
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs.engine({
+    extname: '.hbs',
+    defaultLayout: 'layout.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+const PORT = 8080;
+const server = app.listen(PORT, (req, res) => console.log(`Escuchando el puerto ${PORT}`))
+server.on('error', error => console.log(`Error: ${error}`));
---
+++ desafio_5/pug/src/server.js
@@ -0,0 +1,15 @@
+const usersRoutes = require('./routes/productos.js');
+const express = require('express')
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
+app.use('/', usersRoutes);
+
+app.set('views', './src/views')
+app.set('view engine', 'pug')
+
+const PORT = 8080;
+const server = app.listen(PORT, (req, res) => console.log(`Escuchando el puerto ${PORT}`))
+server.on('error', error => console.log(`Error: ${error}`));
